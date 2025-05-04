092f4fcd884c31277011d4891adf760431112e70
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-05-05 01:33:16 +0800

    feat: enhance DashboardLayout with user context and dynamic sidebar menus

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,19 +1,45 @@
 "use client";
 import Sidebar from "@/components/sidebar/Sidebar";
+import TopBar from "@/components/topbar/TopBar";
 import { CustomChakraProvider } from "@/components/ui/provider";
-import { Flex } from "@chakra-ui/react";
-import React from "react";
+import { ROUTES_LIST } from "@/constants/route-list";
+import { useUserContext } from "@/context/UserContext";
+import { SidebarButton } from "@/interfaces";
+import { Box, Flex, Stack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React, { useMemo } from "react";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  // router
+  const router = useRouter();
+
+  // user context
+  const [{user}] = useUserContext();
+
+  const menus: SidebarButton[] = useMemo(() => {
+    const push = router.push;
+    return ROUTES_LIST.map(({caption: label, url})=>({
+      label,
+      onClick: ()=>push(url ?? "#")
+    })) satisfies SidebarButton[]
+  }, [router.isReady])
+
   return (
     <Flex w={"100%"} h={"100vh"} bg={"gray"}>
-      <Sidebar menus={[{ label: "Dashboard" }]} />
+      {/* Left Side */}
+      <Sidebar topHeader={user?.name} menus={menus} />
 
-      {children}
+      {/* Right Side */}
+      <Stack w={"full"} h={"full"}>
+        <TopBar />
+      <Box px={"8px"}>
+        {children}
+        </Box>
+      </Stack>
     </Flex>
   );
 }
