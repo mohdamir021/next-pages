ee546869c989c33e9fd56a1c1b6011655f19c486
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-05-05 01:33:02 +0800

    feat: add ROUTES_LIST constant for application navigation

---
+++ src/constants/route-list.ts
@@ -0,0 +1,16 @@
+import { CustomRouteType } from "@/interfaces";
+
+export const ROUTES_LIST: CustomRouteType[] = [
+  {
+    caption: "Dashboard",
+    url: "/home",
+  },
+  {
+    caption: "Tasks",
+    url: "/task",
+  },
+  {
+    caption: "Statistic",
+    url: "/stats",
+  },
+];
