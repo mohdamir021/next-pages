5539fe8a98da57abe2b00b8196c728d32553ab48
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-05-05 01:33:21 +0800

    feat: update HomePage to include a heading and layout for DashboardCard

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { NextPageWithLayout } from "./_app";
 import DashboardLayout from "@/layouts/DashboardLayout";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
+import { useUserContext } from "@/context/UserContext";
+import DashboardCard from "@/components/cards/DashboardCard";
 
 const HomePage: NextPageWithLayout = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Heading>Homepage</Heading>
+
+      <Flex my={2} px={2} flexWrap={"wrap"}>
+        <DashboardCard />
+      </Flex>
+    </div>
+  );
 };
 
 HomePage.getLayout = function getLayout(page: React.ReactElement) {
