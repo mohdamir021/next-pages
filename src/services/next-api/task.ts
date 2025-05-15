15ecab1fd3e63e48f1be511b156ed50d6fa22b78
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-05-15 21:09:39 +0800

    fix: fix api task

--- src/services/next-api/task.ts
+++ src/services/next-api/task.ts
@@ -6,15 +6,36 @@ export const taskService = {
       const token = await apiHandler.getAuthToken();
       const reqOptions = {
         headers: {
-          "Authorization": `Bearer ${token}`,
-        }
-      }
-      const response = await apiHandler.get('/task',params,reqOptions);
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await apiHandler.get("/task", params, reqOptions);
 
       return response;
     } catch (error) {
       console.log("task-index", error);
       throw error;
     }
-  }
-}
+  },
+  create: async (payload?: Record<string, unknown>) => {
+    try {
+      const token = await apiHandler.getAuthToken();
+      const reqOptions = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await apiHandler.post(
+        "/task/store",
+        payload,
+        undefined,
+        reqOptions
+      );
+
+      return response;
+    } catch (error) {
+      console.log("task-index", error);
+      throw error;
+    }
+  },
+};
